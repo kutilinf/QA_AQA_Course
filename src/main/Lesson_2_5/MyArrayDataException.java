93cd83968f0cb1b7025e5af6870712d5d55981b3
kutilinf <kutilinf@gmail.com>
2025-10-18 19:45:19 +0300

    Lesson_2_5 домашнее задание.

--- src/main/Lesson_2_5/MyArrayDataException.java
+++ src/main/Lesson_2_5/MyArrayDataException.java
@@ -1,9 +1,6 @@
 package Lesson_2_5;
 
 public class MyArrayDataException extends Exception{
-    private int rows;
-    private int columns;
-    private String value;
 
     public MyArrayDataException(int rows, int columns) {
         super("Данные в ячейке, ["+ rows + "][" + columns + "]" +  "невозможно преобразовать в число.");
