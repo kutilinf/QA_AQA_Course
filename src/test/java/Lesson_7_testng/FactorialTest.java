cb000b1c145f008d72840a99de8afbc4ebffd333
kutilinf <kutilinf@gmail.com>
2025-11-05 00:38:16 +0300

    Домашнее задание по фрэймворку TestNG

--- src/test/java/Lesson_7_testng/FactorialTest.java
+++ src/test/java/Lesson_7_testng/FactorialTest.java
@@ -6,7 +6,11 @@ import static org.testng.Assert.*;
 
 public class FactorialTest {
 
-    @Test
+    @Test(testName = "Проверка правильности вычисления факториала")
     public void testGetFactorial() {
+        Factorial f = new Factorial();
+        int actual = f.getFactorial(5);
+        int expected = 120;
+        assertEquals(expected, actual);
     }
 }
