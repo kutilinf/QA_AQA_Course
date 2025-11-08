0b02689b3f53a1a7d5338bf918d1e0854cf4e2d9
kutilinf <kutilinf@gmail.com>
2025-11-09 02:19:31 +0300

    Создал ветку Lesson_2_8

--- src/main/lesson25/MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-package lesson25;
-
-public class MyArrayDataException extends Exception {
-
-    public MyArrayDataException(int rows, int columns) {
-        super("Данные в ячейке, [" + rows + "][" + columns + "]" + "невозможно преобразовать в число.");
-    }
-
-}
