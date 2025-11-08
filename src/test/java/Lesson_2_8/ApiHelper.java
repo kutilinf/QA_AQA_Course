6628f9369b1b25f7adcadfaeabc699363d4236b9
kutilinf <kutilinf@gmail.com>
2025-11-09 02:54:35 +0300

    Домашнее задание по теме 'Автоматизация тестирования api (+JDBC)'

--- src/test/java/Lesson_2_8/ApiHelper.java
+++ src/test/java/Lesson_2_8/ApiHelper.java
@@ -1,4 +1,58 @@
 package Lesson_2_8;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class ApiHelper {
+
+    public void get() {
+        given().baseUri("https://postman-echo.com")            //Базовый URL
+                .queryParam("foo1", "bar1")           //Параметр запроса
+                .queryParam("foo2", "bar2")           //Параметр запроса
+                .when().get("/get")                            //Метод GET отправляется на эндпоинт /get
+                .then().statusCode(200)                        //Проверяем полученный ответ, что статус 200
+                .body("args.foo1", equalTo("bar1"))    //Проверяем, что в ответе есть поле 'args.foo1' со значением 'bar1'
+                .body("args.foo2", equalTo("bar2"));   //И проверяем, что есть поле 'args.foo2' со значением 'bar2'
+        //.log().all();                                           //Если необходимо посмотреть логи
+    }
+
+    public void post() {
+        given().baseUri("https://postman-echo.com")                                  //Базовый URL
+                .header("Content-Type", "text/plain")                             //Указываем в headers, что передаем простой текст
+                .body("This is expected to be sent back as part of response body.")  //Содержание тела запроса 'This is expected to be sent back as part of response body.'
+                .when().post("/post")                                                //Метод POST отправляется на эндпоинт /post
+                .then().statusCode(200)                                              //Проверяем полученный ответ, что статус 200
+                .body("data", equalTo("This is expected to be sent back as part of response body.")); //Проверяем, что в ответе есть поле 'data' со значением 'This is expected to be sent back as part of response body.'
+        //.log().all();                                                                 //Если необходимо посмотреть логи
+    }
+
+    public void put() {
+        given().baseUri("https://postman-echo.com")                                  //Базовый URL
+                .header("Content-Type", "text/plain")                             //Указываем в headers, что передаем простой текст
+                .body("This is expected to be sent back as part of response body.")  //Содержание тела запроса 'This is expected to be sent back as part of response body.'
+                .when().put("/put")                                                  //Метод PUT отправляется на эндпоинт /put
+                .then().statusCode(200)                                              //Проверяем полученный ответ, что статус 200
+                .body("data", equalTo("This is expected to be sent back as part of response body.")); //Проверяем, что в ответе есть поле 'data' со значением 'This is expected to be sent back as part of response body.'
+        //.log().all();                                                                 //Если необходимо посмотреть логи
+    }
+
+    public void patch() {
+        given().baseUri("https://postman-echo.com")                                  //Базовый URL
+                .header("Content-Type", "text/plain")                             //Указываем в headers, что передаем простой текст
+                .body("This is expected to be sent back as part of response body.")  //Содержание тела запроса 'This is expected to be sent back as part of response body.'
+                .when().patch("/patch")                                              //Метод PATCH отправляется на эндпоинт /patch
+                .then().statusCode(200)                                              //Проверяем полученный ответ, что статус 200
+                .body("data", equalTo("This is expected to be sent back as part of response body.")); //Проверяем, что в ответе есть поле 'data' со значением 'This is expected to be sent back as part of response body.'
+        //.log().all();                                                                 //Если необходимо посмотреть логи
+    }
+
+    public void delete() {
+        given().baseUri("https://postman-echo.com")                                  //Базовый URL
+                .header("Content-Type", "text/plain")                             //Указываем в headers, что передаем простой текст
+                .body("This is expected to be sent back as part of response body.")  //Содержание тела запроса 'This is expected to be sent back as part of response body.'
+                .when().delete("/delete")                                            //Метод DELETE отправляется на эндпоинт /delete
+                .then().statusCode(200)                                              //Проверяем полученный ответ, что статус 200
+                .body("data", equalTo("This is expected to be sent back as part of response body.")); //Проверяем, что в ответе есть поле 'data' со значением 'This is expected to be sent back as part of response body.'
+        //.log().all();                                                                 //Если необходимо посмотреть логи
+    }
 }
--- src/test/java/Lesson_2_8/Test.java
+++ src/test/java/Lesson_2_8/Test.java
@@ -1,4 +1,44 @@
 package Lesson_2_8;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+
 public class Test {
+
+    private static ApiHelper testApi;
+
+    @BeforeAll
+    public static void before() {
+        testApi = new ApiHelper();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест метода GET")
+    public void apiGetTest() {
+        testApi.get();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест метода POST")
+    public void apiPostTest() {
+        testApi.post();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест метода PUT")
+    public void apiPutTest() {
+        testApi.put();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест метода PATCH")
+    public void apiPatchTest() {
+        testApi.patch();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест метода DELETE")
+    public void apiDeleteTest() {
+        testApi.delete();
+    }
 }
